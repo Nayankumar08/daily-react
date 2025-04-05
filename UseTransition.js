41e5ad6eff2b32bb8f57954ac82e0b43d92e64df
Nayan Kumar <136147702+Nayankumar08@users.noreply.github.com>
2025-04-05 11:13:27 +0530

    Add files via upload

--- UseTransition.js
+++ UseTransition.js
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { useTransition } from "react";
 
 function App() {
   const [name, setName] = React.useState("");
   const [lists, setLists] = React.useState([]);
+  const [isPending, startTransition] = useTransition();
   const LIST_SIZE = 20000;
   const handleChange = (e) => {
     const { value } = e.target;
     setName(value);
-    const datalist = [];
-    for (let i = 0; i < LIST_SIZE; i++) {
-      datalist.push(value);
-    }
-    setLists(datalist);
+
+    startTransition(() => {
+      const datalist = [];
+      for (let i = 0; i < LIST_SIZE; i++) {
+        datalist.push(value);
+      }
+      setLists(datalist);
+    });
   };
 
   return (
     <div>
       <input type="text" values={name} onChange={handleChange} key="input" />
-      {lists.map((list, index) => {
-        return <div key={index}>{list}</div>;
-      })}
+      {isPending ? (
+        <div>Loading</div>
+      ) : (
+        lists.map((list, index) => {
+          return <div key={index}>{list}</div>;
+        })
+      )}
     </div>
   );
 }
